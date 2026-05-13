4104fbb332b0df8316c88a720956ae8b380488ff
George Norton <george_norton_uk@hotmail.com>
2026-05-13 14:21:23 +0100

    Tweak acceleration curve again.

--- keyboards/ploopyco/bean/keymaps/maccel/config.h
+++ keyboards/ploopyco/bean/keymaps/maccel/config.h
@@ -1,4 +1,4 @@
 #define POINTING_DEVICE_ACCEL_TAKEOFF 1.0      // lower/higher value = curve takes off more smoothly/abruptly
 #define POINTING_DEVICE_ACCEL_GROWTH_RATE 0.4  // lower/higher value = curve reaches its upper limit slower/faster
 #define POINTING_DEVICE_ACCEL_OFFSET 6.0       // lower/higher value = acceleration kicks in earlier/later
-#define POINTING_DEVICE_ACCEL_LIMIT 0.2        // lower limit of accel curve (minimum acceleration factor)
+#define POINTING_DEVICE_ACCEL_LIMIT 0.1        // lower limit of accel curve (minimum acceleration factor)
